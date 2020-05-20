040f62c8408c5d853f4b935599ccfb4e4670b273
Gustavo <gustavo.goncalvespires@gmail.com>
2020-05-20 20:10:49 -0300

    Initial commit + gitignore file

---
+++ PixelCity/Controller/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  MapVC.swift
+//  PixelCity
+//
+//  Created by Gustavo Mac Mini on 20/05/20.
+//  Copyright © 2020 DEVX. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
