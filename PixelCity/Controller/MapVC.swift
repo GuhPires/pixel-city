d0466cb635d020d9211b55618f5c4a0e356a1a55
Gustavo <gustavo.goncalvespires@gmail.com>
2020-05-20 21:22:16 -0300

    Asking user to authorize using location + Center in user Location

--- PixelCity/Controller/MapVC.swift
+++ PixelCity/Controller/MapVC.swift
@@ -8,25 +8,56 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
     
     // MARK: - Outlets
     @IBOutlet weak var mapView: MKMapView!
     
+    // MARK: - Global variables
+    var locationManager = CLLocationManager()
+    let authStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mapView.delegate = self
+        locationManager.delegate = self
+        
+        configureLocationServices()
     }
 
     // MARK: - Actions
     @IBAction func onCenterMapPressed(_ sender: Any) {
+        if authStatus == .authorizedAlways || authStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
     }
     
 }
 
 // MARK: - Map View methods
 extension MapVC: MKMapViewDelegate {
+    func centerMapOnUserLocation() {
+        guard let coord = locationManager.location?.coordinate else { return }
+        let coordRegion = MKCoordinateRegion(center: coord, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
+        mapView.setRegion(coordRegion, animated: true)
+    }
+}
+
+// MARK: - Core Location methods
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if authStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
     
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
 }
