e3ddb601eea0dfda15e3a136cd856869aa0ddbc3
Gustavo <gustavo.goncalvespires@gmail.com>
2020-05-20 23:53:23 -0300

    Created Flickr URL function to retrieve its images

--- PixelCity/Utilities/Constants.swift
+++ PixelCity/Utilities/Constants.swift
@@ -9,3 +9,7 @@
 import Foundation
 
 let API_KEY = FLICKR_KEY
+
+func flickrURL(withAnnotation annotation: DroppablePin, numberOfPhotos number: Int) -> String {
+    return "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(API_KEY)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=5&radius_units=km&per_page=\(number)&format=json&nojsoncallback=1"
+}
